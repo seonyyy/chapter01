abc1321993af7cb09e7c95b137b87be07aab3e74
T3AD20 <T3AD20@T3AD20C>
2017-01-04 14:55:36 +0900

    소수판단 - SOLVED

--- src/chapter01/PrimeDetect.java
+++ src/chapter01/PrimeDetect.java
@@ -12,7 +12,21 @@ public class PrimeDetect {
 		int number = scanner.nextInt();
 		
 		// 판단 알고리즘
+		int i = 2;
+		boolean isPrime = true;
+		while( i < number - 1 ) {
+			if( number % i == 0 ) {
+				isPrime = false;
+				break;
+			}
+			i++;
+		}
 		
 		//출력
+		if( isPrime ) {
+			System.out.println( "소수입니다." );
+		} else {
+			System.out.println( "소수가 아닙니다." );
+		}
 	}
 }
