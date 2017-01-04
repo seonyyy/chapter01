0ed44d016f3a65d2f0e8f7225792905e720f349e
T3AD20 <T3AD20@T3AD20C>
2017-01-04 11:39:25 +0900

    for문 연습

---
+++ src/chapter01/ForTest.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+public class ForTest {
+
+	public static void main(String[] args) {
+		// 1~10까지 합을 구한다.
+		int sum = 0;
+		for( int i = 1; i <= 10; i++ ) {
+			//System.out.println( i );
+			sum = sum + i;
+		}
+		System.out.println( sum );
+	}
+
+}
