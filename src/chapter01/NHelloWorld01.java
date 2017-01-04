a40397e7b623dce3199c4b45cdff1e2b5acb7570
T3AD20 <T3AD20@T3AD20C>
2017-01-04 10:26:16 +0900

    NHelloWorld01, PowerOfTwo01 완성.

---
+++ src/chapter01/NHelloWorld01.java
@@ -0,0 +1,22 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class NHelloWorld01 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
+		
+		System.out.print( "count:");
+		int count = scanner.nextInt();
+		
+		int i = 0;
+		while( i < count ) {
+			System.out.println( 
+				"[" + (i+1) + "] Hello World"    );
+			i++;
+		}
+
+	}
+
+}
---
+++ src/chapter01/PowerOfTwo01.java
@@ -0,0 +1,26 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class PowerOfTwo01 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
+
+		int result = 1;
+		System.out.print("승수: ");
+
+		int pow = scanner.nextInt();
+		
+		// Q: 아래 while 반복문을 작성하시오.
+		int i = 0;
+		while( i < pow ) {
+			result = result * 2;
+			i++;
+		}
+		
+		System.out.println("2의 " + pow + "제곱근은 " + result + "입니다.");
+		
+		scanner.close();
+	}
+}
