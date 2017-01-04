7fe32d141739bcd41fd80cddfef4e5a9068b3eec
T3AD20 <T3AD20@T3AD20C>
2017-01-04 14:51:33 +0900

    break & continue문 연습

---
+++ src/chapter01/BreakTest.java
@@ -0,0 +1,19 @@
+package chapter01;
+
+public class BreakTest {
+
+	public static void main(String[] args) {
+		int num = 1;
+		int sum = 0;
+		while( true ) {
+			sum = sum + num;
+			if( sum > 5000 ) {
+				break;
+			}
+			num++;
+		}
+
+		System.out.println( sum );
+	}
+
+}
---
+++ src/chapter01/ContinueTest.java
@@ -0,0 +1,24 @@
+package chapter01;
+
+public class ContinueTest {
+
+	public static void main(String[] args) {
+		
+		for( int i = 0; i <= 20; i++ ) {
+			
+//			if( i % 2 != 0 ) {
+//				System.out.println( i );
+//			}
+			if( i % 2 == 0 ) {
+				continue;
+			}
+
+			if( i % 3 == 0 ) {
+				continue;
+			}
+			
+			System.out.println( i );
+		}
+	}
+
+}
