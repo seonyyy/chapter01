a87cde5faa2f0ae97b9ca02cdbe9cb9dd089c374
T3AD20 <T3AD20@T3AD20C>
2017-01-04 11:39:11 +0900

    소수판별 - skelton

---
+++ src/chapter01/PrimeDetect.java
@@ -0,0 +1,18 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class PrimeDetect {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner( System.in );
+		System.out.print( "소수인지 판단할 숫자:" );
+		
+		int number = scanner.nextInt();
+		
+		// 판단 알고리즘
+		
+		//출력
+	}
+}
