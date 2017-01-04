2ef99de4af90c16c387724ea38ecc7e48174422b
T3AD20 <T3AD20@T3AD20C>
2017-01-04 15:01:58 +0900

    Gugudan - 수정

--- src/chapter01/Gugudan.java
+++ src/chapter01/Gugudan.java
@@ -5,7 +5,9 @@ public class Gugudan {
 	public static void main(String[] args) {
 		for( int i = 1; i <= 9; i++ ) {
 			for( int j = 1; j <= 9; j++ ) {
-				System.out.print( j + " * " + i + " = " + (j*i) );
+				int result = j * i;
+				
+				System.out.print( j + " * " + i + " = " + result );
 				System.out.print( "\t" );
 			}
 			
