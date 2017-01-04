fa22153ef379f42f83443f0ba29f289a7044f833
T3AD20 <T3AD20@T3AD20C>
2017-01-04 13:00:28 +0900

    NHelloWorld2 이름변경

--- src/chapter01/NHelloWorld02.java
+++ src/chapter01/NHelloWorld02.java
@@ -2,7 +2,7 @@ package chapter01;
 
 import java.util.Scanner;
 
-public class NHelloWorld2 {
+public class NHelloWorld02 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
